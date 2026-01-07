390582425e87136f75d2c4e1320493d238546884
BHAVANA SAHU <bvasahu@gmail.com>
2026-01-07 22:04:19 +0530

    Add initial graph representation code

---
+++ graph.py
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+/////////////////////////////// Adjaceny Mtrix Representation of Graph unwaighted or undirected///////////////////
+// char arr[5]{'A','B','C','D','E'};
+// int graph[5][5]={0};
+// int main(){
+//     char u,v;
+//     for (int i=0;i<6;i++){
+//         cin>>u>>v;
+//         int ui=u-'A';
+//         int vi=v-'A';
+//         graph[ui][vi]=1;
+//         graph[vi][ui]=1;
+
+//     }
+//     printf("  ");
+//     for(int i = 0; i < 5; i++)
+
+//         printf("%c ", arr[i]);
+
+//     printf("\n");
+
+//     for(int i = 0; i < 5; i++) {
+//         printf("%c ", arr[i]);
+//         for(int j=0;j<5;j++){
+//             cout<<graph[i][j]<<" ";
+//         }
+//         cout<<endl;
+        
+//     }
+
+
+// }
