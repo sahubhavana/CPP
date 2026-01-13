e38a18626825e0b0062f7cde964c0f5f62001f2c
BHAVANA SAHU <bvasahu@gmail.com>
2026-01-13 17:52:08 +0530

    Create circularaddbeg.cpp

---
+++ circularaddbeg.cpp
@@ -0,0 +1,12 @@
+void addbeg(int val){
+    temp=first;
+    while(temp->next!=first){
+        temp=temp->next;
+    }
+    ttemp=new cll;
+    ttemp->data=val;
+    ttemp->next=first;
+    temp->next=ttemp;
+    first=ttemp;
+
+}
