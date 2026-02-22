e268a458487fe6b5d3181bc8f19e01638eebbd1b
BHAVANA SAHU <bvasahu@gmail.com>
2026-02-22 22:03:48 +0530

    Add files via upload

---
+++ for greater 10 number.cpp
@@ -0,0 +1,171 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+   clrscr();
+   int a, b, c, d ,e, f, g, h, i, j;
+   printf(" enter value of n=");
+   scanf(" %d%d%d%d%d%d%d%d%d%d", &a, &b, &c, &d, &e, &f, &g, &h, &i, &j);
+   if(a>b){
+     if(a>c){
+       if(a>d){
+         if(a>e){
+           if(a>f){
+             if(a>g){
+               if(a>h){
+                 if(a>i){
+                   if(a>j){
+                     printf("greater number is a");
+             }else{
+                    printf("greater number is  j");
+             }
+              
+           }
+         }
+       }
+     }
+   }
+  }
+ }
+ } 
+   else{ 
+        if(b>c){
+          if(b>d){
+            if(b>e){
+              if(b>f){
+                if(b>g){
+                  if(b>h){
+                    if(b>i){
+                      if(b>j){
+                       printf("greater number is b");
+                }
+                else{
+                     printf(" greater number is j");
+                }
+              }
+            }
+          }
+        }
+      }  
+     } 
+   }    else{
+            if(c>d){
+              if(c>e){
+                if(c>f){
+                  if(c>g){
+                   if(c>h){
+                     if(c>i){
+                       if(c>j){
+                          printf (" greater number is c");
+                  
+                  }
+                  else{
+                       printf(" greater number is j");
+                  }
+                }
+              }
+            }
+          }
+       } 
+     }      
+            else{
+                  if(d>e){
+                     if(d>f){
+                        if(d>g){
+                         if(d>h){
+                          if(d>i){
+                            if(d>j){
+                               printf(" greater number is d");
+                               }
+                        else{
+                              printf(" greater number is j");
+                            }
+                          }
+                        }
+                      }
+                     } 
+                  }   
+                  else{
+                       if(e>f){
+                         if(e>g){
+                           if(e>h){
+                             if(e>i){
+                               if(e>j){
+                                 printf (" greater number is e");
+                                  }
+                         else{
+                              printf(" greater number is  j");
+                
+                             }
+                          }
+                        }  
+                        }
+                        }  
+                       
+                       else{
+                           if(f>g){
+                             if(f>h){
+                               if(f>i){
+                                 if(f>j){
+                               printf (" greater number is f");
+              
+                                }
+                           else{
+                              printf (" greater number is j");
+                              }
+                              }
+                              }   
+                           }   
+                           else{ 
+                              if(g>h){
+                                if(g>i){
+                                  if(g>j){
+                                     printf (" greater number is g");
+                                  }
+                                      else{
+                                          printf(" greater number is j");
+                                      }
+                                      
+                                  }
+                                }
+                              
+                              else{
+                                 if(h>i){
+                                   if(h>j){
+                                      printf (" greater number id h");
+                                      }
+                                   
+                                       else{
+                                          printf (" graeter number is j");
+                                       }
+                                   }
+                                 
+                                 else{
+                                     if(i>j){
+                                            printf (" greater number is i");
+                                     }
+                                         else{
+                                              printf (" greater number is j");
+                                         }
+                                     }
+                                 }
+                              }     
+                           }   
+                       }
+                             
+                    }
+                  }  
+   
+             }
+             
+   getch();
+}   
+   
+
+
+
+
+
+
+
+    
