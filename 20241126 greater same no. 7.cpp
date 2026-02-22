960000d1c7e49f850abd29a97c57ff13dc745048
BHAVANA SAHU <bvasahu@gmail.com>
2026-02-22 22:03:36 +0530

    Add files via upload

---
+++ 20241126 greater same no. 7.cpp
@@ -0,0 +1,135 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+   clrscr();
+   int a, b, c, d ,e, f, g;
+   printf(" enter value of n=");
+   scanf(" %d%d%d%d%d%d%d", &a, &b, &c, &d, &e, &f, &g);
+   if(a>b){
+     if(a>c){
+       if(a>d){
+         if(a>e){
+           if(a>f){
+             if(a>g){
+                     printf("greater number is a");
+             }else{
+                    if(a=b=c=d=e=f=g){
+                         printf(" all are equal");
+                    }
+                    else{
+                   
+                    printf("greater number is  g");
+                    }
+             }
+              
+           }
+         }
+       }
+     }
+   }
+   else{ 
+        if(b>c){
+          if(b>d){
+            if(b>e){
+              if(b>f){
+                if(b>g){
+                       printf("greater number is b");
+                }
+                else{
+                     if( a=b=c=d=e=f=g){
+                        printf(" all are equal");
+                     }
+                     else{
+                     printf(" greater number is g");
+                     }
+                }
+              }
+            }
+          }
+        }
+        else{
+            if(c>d){
+              if(c>e){
+                if(c>f){
+                  if(c>g){
+                          printf (" greater number is c");
+                  
+                  }
+                  else{
+                       if(a=b=c=d=e=f=g){
+                       printf(" all are equal ");
+                       }
+                       else{
+                          printf(" greater number is g");
+                  }
+                }
+              }
+            }
+            }
+        
+            else{
+                  if(d>e){
+                     if(d>f){
+                        if(d>g){
+                               printf(" greater number is d");
+                               }
+                        else{
+                              if(a=b=c=d=e=f=g){
+                              printf (" all are equal");
+                              }
+                              
+                             else{
+                              printf(" greater number is g");
+                              }
+                            }
+                          }
+                        }
+                      
+                  else{
+                       if(e>f){
+                         if(e>g){
+                                 printf (" greater number is e");
+                                  }
+                         else{
+                              if(a=b=c=d=e=f=g){
+                                 printf("all are equal");
+                              }
+                              else{
+                              printf(" greater number is  g");
+                              }
+                
+                             }
+                          }
+                       
+                       else{
+                           if(f>g){
+                               printf (" greater number is f");
+              
+                                }
+                           else{
+                                if(a=b=c=d=e=f=g){
+                                   printf("all are equal");
+                                }
+                                else{
+                           
+                                   printf (" greater number is g");
+                                   }
+                              }
+                           }   
+                       }
+                             
+                    }
+             }       
+   }          
+   getch();
+}   
+   
+
+
+
+
+
+
+
+    
