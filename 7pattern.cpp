8c720f73c4cedc51f0f87b92784bf8f13222d156
BHAVANA SAHU <bvasahu@gmail.com>
2026-04-10 10:39:33 +0530

    Create 7pattern.cpp

---
+++ 7pattern.cpp
@@ -0,0 +1,11 @@
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            char ch='A'+(i-1);
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+}
