bebe1a71398eac8cfc0047acfade6e13dcaa0316
BHAVANA SAHU <bvasahu@gmail.com>
2026-04-10 10:36:25 +0530

    Create 4pattern.py

---
+++ 4pattern.py
@@ -0,0 +1,13 @@
+// int main(){
+//     int n;
+//     int count=1;
+//     cin>>n;
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<=i;j++){
+//            cout<< count <<" ";
+
+//         }
+//         count=count+1;
+//         cout<<endl;
+//     }
+// }
