90858d71c795c583af7b95265aa05f75f35c6c81
BHAVANA SAHU <bvasahu@gmail.com>
2026-04-10 10:35:04 +0530

    Remove commented-out C++ code from 2pattern.py

---
+++ 2pattern.py
@@ -0,0 +1,16 @@
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=n){
+//             cout<<j;
+//             j++;
+
+//         }
+//          cout<<endl;
+//          i++;
+//     }
+// }
